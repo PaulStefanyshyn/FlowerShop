6d002f68bffb9560880f264d7ee9fa7aba5cb18a
PaulStefanyshyn <126797697+PaulStefanyshyn@users.noreply.github.com>
2026-05-13 12:21:39 +0300

    aptate burger logic

---
+++ static/js/burger.js
@@ -0,0 +1,24 @@
+const burger = document.querySelector('.burger');
+const navbar = document.querySelector('.navbar');
+const overlay = document.querySelector('.overlay');
+const links = document.querySelectorAll('.navbar a');
+
+function closeMenu() {
+    burger.classList.remove('active');
+    navbar.classList.remove('active');
+    overlay.classList.remove('active');
+    document.body.classList.remove('no-scroll');
+}
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    navbar.classList.toggle('active');
+    overlay.classList.toggle('active');
+    document.body.classList.toggle('no-scroll');
+});
+
+overlay.addEventListener('click', closeMenu);
+
+links.forEach(link => {
+    link.addEventListener('click', closeMenu);
+});
